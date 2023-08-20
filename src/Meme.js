e4537eead52d6ea7f8a1e0684fdd6a4fe85cc34a
Lurie <lurainekimmerle@gmail.com>
2023-08-20 05:48:00 +0000

    merge input1 and input 2 to one css rule and add text to input

--- src/Meme.js
+++ src/Meme.js
@@ -2,8 +2,8 @@ export default function Meme (){
 
     return (
         <form className="input-form">
-            <input className="input1" type="text"></input>
-            <input className="input2" type="text"></input>
+            <input className="input1" value="Shut up" type="text"></input>
+            <input className="input2" value="and take my money" type="text"></input>
             <button className="submit-btn" type="submit">Get a new meme image  🖼</button>
         </form>
 
