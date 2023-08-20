76b10ed91fe6fc84fc050897857c727beff8eaa3
Lurie <lurainekimmerle@gmail.com>
2023-08-20 06:45:01 +0000

    test log memes array

--- src/Meme.js
+++ src/Meme.js
@@ -1,18 +1,24 @@
-export default function Meme (){
+import data from "./data.js"
 
+export default function Meme (){
+    let memesArr= data.data.memes
+    function newMeme(){
+        console.log(memesArr)
+    }
     return (
-        <InputDeviceInfo className="input-form">
+        <main className="input-form">
             <div className="input-fields">
                 <input  className="input1" value="Shut up" type="text"></input>
                 <input  className="input2" value="and take my money" type="text"></input>
             </div>
             <button 
             
-            onClick={}
+            onClick={newMeme}
             className="submit-btn" 
             type="submit"
             >Get a new meme image  🖼</button>
-        </div>
+    
+        </main>
 
     )
 }
