40feab38ee84af26d80f2644a554d2efa245dd6f
Lurie <lurainekimmerle@gmail.com>
2023-08-20 05:09:15 +0000

    add content to Header.js

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,11 @@ import Troll from "./imgs/Troll_Face.png"
 
 export default function Header (){
     <section>
-        <img src={Troll}/>
+        <div className="title">
+            <img src={Troll}/>
+            <h1>Meme Generator</h1>
+        </div>
+        <h3>React Course - Project 3<h3/>
 
     </section>
 
