293a00aac1756d261862ff798365c96ef714cb19
Lurie <lurainekimmerle@gmail.com>
2023-08-20 06:30:35 +0000

    organize button for function

--- src/Meme.js
+++ src/Meme.js
@@ -1,13 +1,18 @@
 export default function Meme (){
 
     return (
-        <form className="input-form">
+        <InputDeviceInfo className="input-form">
             <div className="input-fields">
                 <input  className="input1" value="Shut up" type="text"></input>
                 <input  className="input2" value="and take my money" type="text"></input>
             </div>
-            <button className="submit-btn" type="submit">Get a new meme image  🖼</button>
-        </form>
+            <button 
+            
+            onClick={}
+            className="submit-btn" 
+            type="submit"
+            >Get a new meme image  🖼</button>
+        </div>
 
     )
 }
