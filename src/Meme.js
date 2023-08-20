93fa0e21886bbf384b6c99388eba2b9ae541cc33
Lurie <lurainekimmerle@gmail.com>
2023-08-20 06:09:12 +0000

    center input fields and buton

--- src/Meme.js
+++ src/Meme.js
@@ -2,8 +2,10 @@ export default function Meme (){
 
     return (
         <form className="input-form">
-            <input className="input1" value="Shut up" type="text"></input>
-            <input className="input2" value="and take my money" type="text"></input>
+            <div className="input-fields">
+                <input  className="input1" value="Shut up" type="text"></input>
+                <input  className="input2" value="and take my money" type="text"></input>
+            </div>
             <button className="submit-btn" type="submit">Get a new meme image  🖼</button>
         </form>
 
