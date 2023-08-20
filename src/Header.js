479fe401a22ce5dfa393206187d1c0c57c837cef
Lurie <lurainekimmerle@gmail.com>
2023-08-20 05:12:14 +0000

    add background to header

--- src/Header.js
+++ src/Header.js
@@ -3,12 +3,12 @@ import Troll from "./imgs/Troll_Face.png"
 export default function Header (){
    
    return ( 
-   <section>
+   <section className="header">
         <div className="title">
             <img src={Troll}/>
             <h1>Meme Generator</h1>
         </div>
-        <h3>React Course - Project 3</h3>
+        <h3 className="subtitle">React Course - Project 3</h3>
     </section>
 )
 }
