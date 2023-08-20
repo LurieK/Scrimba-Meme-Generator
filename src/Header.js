430984e0f5aaf042e1303ee456ecbc181f6cc26a
Lurie <lurainekimmerle@gmail.com>
2023-08-20 05:10:15 +0000

    add return and fix syntax error

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,14 @@
 import Troll from "./imgs/Troll_Face.png"
 
 export default function Header (){
-    <section>
+   
+   return ( 
+   <section>
         <div className="title">
             <img src={Troll}/>
             <h1>Meme Generator</h1>
         </div>
-        <h3>React Course - Project 3<h3/>
-
+        <h3>React Course - Project 3</h3>
     </section>
-
+)
 }
